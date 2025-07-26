6f206fa74740ff2b8b5816dcf95ca0fd0563c6a1
portuna85 <portuna85@gmail.com>
2025-07-26 14:55:25 +0900

    start

---
+++ src/test/java/com/kraft/logistics/KraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kraft.logistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KraftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
