eadf1ef7742413dab88f5ebf73ab9e8f1b4814bf
portuna85 <portuna85@gmail.com>
2025-08-09 15:45:21 +0900

    mustache 작성

--- src/main/java/com/kraft/book/service/PostsService.java
+++ src/main/java/com/kraft/book/service/PostsService.java
@@ -2,12 +2,16 @@ package com.kraft.book.service;
 
 import com.kraft.book.domain.posts.Posts;
 import com.kraft.book.domain.posts.PostsRepository;
+import com.kraft.book.web.dto.PostsListResponseDto;
 import com.kraft.book.web.dto.PostsResponseDto;
 import com.kraft.book.web.dto.PostsSaveRequestDto;
 import com.kraft.book.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -33,4 +37,16 @@ public class PostsService {
         return new PostsResponseDto(entity);
     }
 
+    @Transactional
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+        postsRepository.delete(posts);
+    }
 }
--- src/main/java/com/kraft/book/web/IndexController.java
+++ src/main/java/com/kraft/book/web/IndexController.java
@@ -1,14 +1,35 @@
 package com.kraft.book.web;
 
+import com.kraft.book.service.PostsService;
+import com.kraft.book.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
-    @GetMapping(value = "/", produces = "text/html; charset=UTF-8")
-    public String index() {
+    private final PostsService postsService;
 
+    @GetMapping(value = "/", produces = "text/html; charset=UTF-8")
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index"; // returns the index.html file located in src/main/resources/templates
     }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save"; // returns the posts-save.html file located in src/main/resources/templates
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts-update"; // returns the posts-update.html file located in src/main/resources/templates
+    }
+
 }
