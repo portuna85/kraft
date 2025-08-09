a3d9f601cd3b03ab1d1c50e55bd4762466cf0e66
portuna85 <portuna85@gmail.com>
2025-08-09 17:09:01 +0900

    Thymeleaf로 프론트 수정

--- build/resources/main/static/js/app/index.js
+++ build/resources/main/static/js/app/index.js
@@ -1,75 +1,106 @@
-var main = {
-    init : function () {
-        var _this = this;
-        $('#btn-save').on('click', function () {
-            _this.save();
-        });
+// /static/js/app/index.js
+(function () {
+    const $doc = $(document);
 
-        $('#btn-update').on('click', function () {
-            _this.update();
-        });
+    // CSRF 세팅
+    const csrfToken = $('meta[name="_csrf"]').attr('content');
+    const csrfHeader = $('meta[name="_csrf_header"]').attr('content');
+    $.ajaxSetup({
+        beforeSend: function (xhr) {
+            if (csrfToken && csrfHeader) xhr.setRequestHeader(csrfHeader, csrfToken);
+        }
+    });
 
-        $('#btn-delete').on('click', function () {
-            _this.delete();
-        });
-    },
-    save : function () {
-        var data = {
-            title: $('#title').val(),
-            author: $('#author').val(),
-            content: $('#content').val()
-        };
-
-        $.ajax({
-            type: 'POST',
-            url: '/api/v1/posts',
-            dataType: 'json',
-            contentType:'application/json; charset=utf-8',
-            data: JSON.stringify(data)
-        }).done(function() {
-            alert('글이 등록되었습니다.');
-            window.location.href = '/';
-        }).fail(function (error) {
-            alert(JSON.stringify(error));
-        });
-    },
-    update : function () {
-        var data = {
-            title: $('#title').val(),
-            content: $('#content').val()
-        };
-
-        var id = $('#id').val();
-
-        $.ajax({
-            type: 'PUT',
-            url: '/api/v1/posts/'+id,
-            dataType: 'json',
-            contentType:'application/json; charset=utf-8',
-            data: JSON.stringify(data)
-        }).done(function() {
-            alert('글이 수정되었습니다.');
-            window.location.href = '/';
-        }).fail(function (error) {
-            alert(JSON.stringify(error));
-        });
-    },
-    delete : function () {
-        var id = $('#id').val();
-
-        $.ajax({
-            type: 'DELETE',
-            url: '/api/v1/posts/'+id,
-            dataType: 'json',
-            contentType:'application/json; charset=utf-8'
-        }).done(function() {
-            alert('글이 삭제되었습니다.');
-            window.location.href = '/';
-        }).fail(function (error) {
-            alert(JSON.stringify(error));
-        });
+    // 공통: 알림 + 이동
+    function goHome(msg) {
+        if (msg) alert(msg);
+        window.location.assign('/');
     }
 
-};
+    // 간단 검증
+    function required(val) { return val != null && String(val).trim().length > 0; }
 
-main.init();
+    const main = {
+        init: function () {
+            $doc.on('click', '#btn-save', main.save);
+            $doc.on('click', '#btn-update', main.update);
+            $doc.on('click', '#btn-delete', main.remove);
+        },
+
+        save: function () {
+            const $btn = $('#btn-save').prop('disabled', true);
+            const data = {
+                title: $('#title').val(),
+                author: $('#author').val(),
+                content: $('#content').val()
+            };
+
+            if (!required(data.title) || !required(data.author) || !required(data.content)) {
+                alert('제목/작성자/내용을 모두 입력하세요.');
+                return void $btn.prop('disabled', false);
+            }
+
+            $.ajax({
+                method: 'POST',
+                url: '/api/v1/posts',
+                dataType: 'json',
+                contentType: 'application/json; charset=utf-8',
+                data: JSON.stringify(data)
+            })
+                .done(() => goHome('글이 등록되었습니다.'))
+                .fail(err => alert(JSON.stringify(err)))
+                .always(() => $btn.prop('disabled', false));
+        },
+
+        update: function () {
+            const $btn = $('#btn-update').prop('disabled', true);
+            const id = $('#id').val();
+            const data = {
+                title: $('#title').val(),
+                content: $('#content').val()
+            };
+
+            if (!required(id)) {
+                alert('잘못된 요청입니다: id 없음');
+                return void $btn.prop('disabled', false);
+            }
+            if (!required(data.title) || !required(data.content)) {
+                alert('제목/내용을 입력하세요.');
+                return void $btn.prop('disabled', false);
+            }
+
+            $.ajax({
+                method: 'PUT',
+                url: '/api/v1/posts/' + encodeURIComponent(id),
+                dataType: 'json',
+                contentType: 'application/json; charset=utf-8',
+                data: JSON.stringify(data)
+            })
+                .done(() => goHome('글이 수정되었습니다.'))
+                .fail(err => alert(JSON.stringify(err)))
+                .always(() => $btn.prop('disabled', false));
+        },
+
+        remove: function () {
+            const $btn = $('#btn-delete').prop('disabled', true);
+            const id = $('#id').val();
+            if (!required(id)) {
+                alert('잘못된 요청입니다: id 없음');
+                return void $btn.prop('disabled', false);
+            }
+            if (!confirm('정말 삭제하시겠습니까?')) {
+                return void $btn.prop('disabled', false);
+            }
+
+            $.ajax({
+                method: 'DELETE',
+                url: '/api/v1/posts/' + encodeURIComponent(id)
+            })
+                .done(() => goHome('글이 삭제되었습니다.'))
+                .fail(err => alert(JSON.stringify(err)))
+                .always(() => $btn.prop('disabled', false));
+        }
+    };
+
+    main.init();
+})();
